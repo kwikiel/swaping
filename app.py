8c3c6529ed1d48c0bccbf684fd8e1ccac67102b5
Kacper <random8902@gmail.com>
2015-05-09 22:29:29 +0200

    Feature: Bitmarket class for API

--- app.py
+++ app.py
@@ -1,23 +1,66 @@
 #Import flask
 from flask import Flask
 from flask import render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+import hashlib
+
+#Library for bitmarket
+from swaper import *
+
+
 #Make app instance
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Keys(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_key = db.Column(db.String(256), unique=False)
+    private_key = db.Column(db.String(256), unique=True)
+    url = db.Column(db.String(256), unique=True)
+
+    def __init__(self, public_key, private_key):
+        self.public_key = public_key
+        self.private_key = private_key
+        self.url = hashlib.sha256('{0.public_key}{0.private_key}lol'.format(self)).hexdigest()
+
+    def __repr__(self):
+        return "<Public:  {public} Prv: {private}, {url}>".format(
+            public=self.public_key, private=self.private_key, url=self.url)
+
 
 #Basic route for index
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+
 @app.route('/handle', methods=['GET', 'POST'])
 def handle():
     if request.method == 'POST':
-        return request.form['pubkey']
+        if request.form['pubkey'] and request.form['privkey']:
+            keys = Keys(request.form['pubkey'], request.form['privkey'])
+            db.session.add(keys)
+            db.session.commit()
+            return hashlib.sha256('{public_key}{private_key}lol'.format(public_key=request.form['pubkey'], private_key=request.form['privkey'])).hexdigest()
+        else:
+            return "Something wrong with data"
+    else:
+        return "Go to login page again, sorry"
 
+@app.route('/display/<special>')
+def display(special):
+    #Query database for keys
+    record = Keys.query.filter(Keys.url == special).first()
+    #Class initialisation for bitmarket API
+    xD
+    return render_template("display.html", record=record)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ swaper.py
@@ -0,0 +1,68 @@
+import hmac
+import time
+import urllib
+import hashlib
+import requests
+
+import config
+
+
+class BitapiException(Exception):
+    pass
+
+class Yolo():
+
+    def __init__(self, key=config.key, secret=config.secret):
+        self.key = key
+        self.secret = secret
+
+    def bitapi(self, method, **params):
+        """Main method for bitmarket API"""
+        endpoint = "https://www.bitmarket.pl/api2/"
+        times = int(time.time())
+        params.update({
+            "method": method,
+            "tonce": times,
+            "currency": "BTC"
+            })
+
+        post = urllib.urlencode(params)
+        sign = hmac.HMAC(self.secret, post, digestmod=hashlib.sha512).hexdigest()
+        headers = {"API-Key": self.key, "API-Hash": sign}
+        raw = requests.post(endpoint, data=post, headers=headers)
+
+        if 'error' in raw.json():
+            raise BitapiException(raw.json())
+        return raw.json()
+
+
+    def get_cutoff(self):
+        """Returns max profit for swaps """
+        raw = requests.get("http://bitmarket.pl/json/swapBTC/swap.json")
+        return raw.json()["cutoff"]
+
+
+    def cancel_all(self):
+        """Powerful: cancels all open swaps """
+        swap_list = bitapi("swapList")["data"]
+
+        for swap in swap_list:
+            bitapi("swapClose", id=swap["id"])
+
+
+    def make_best(self):
+        # Uses magic contant, remove later.
+        return self.bitapi('swapOpen', amount=(float(get_balance())-0.01), rate=float(get_cutoff()-0.05))
+
+
+    def swap_list(self):
+        """Listing all open offers with nice interface"""
+        swap_list_data = self.bitapi("swapList")["data"]
+        return swap_list_data
+
+
+    def get_info(self):
+        return self.bitapi('info')
+
+    def get_balance(self):
+        return self.bitapi('info')['data']['balances']['available']['BTC']
