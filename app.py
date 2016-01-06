1001e903a7f9914cfccd4d937558e5d8256a0a6e
Kacper Wikiel <random8902@gmail.com>
2016-01-06 20:20:25 +0100

    PEP8 heroku gunicorn

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-#Import flask
+# Import flask
 from flask import Flask
-from flask import render_template, request, make_response
+from flask import render_template, request, make_response, redirect
+from flask import current_app
 from flask.ext.sqlalchemy import SQLAlchemy
 import hashlib
 import os.path
-#Library for bitmarket
-from swaper import *
-
-
-#Make app instance
+# Library for bitmarket
+from swaping import Yolo
+from swaping import interest
+# Make app instance
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
@@ -23,14 +23,15 @@ class Keys(db.Model):
     def __init__(self, public_key, private_key):
         self.public_key = public_key.encode('utf8')
         self.private_key = private_key.encode('utf8')
-        self.url = hashlib.sha256('{0.public_key}{0.private_key}lol'.format(self)).hexdigest()
+        self.url = hashlib.sha256
+        ('{0.public_key}{0.private_key}lol'.format(self)).hexdigest()
 
     def __repr__(self):
         return "<Public:  {public} Prv: {private}, {url}>".format(
             public=self.public_key, private=self.private_key, url=self.url)
 
 
-#Basic route for index
+# Basic route for index
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -48,49 +49,52 @@ def handle():
             keys = Keys(request.form['pubkey'], request.form['privkey'])
             db.session.add(keys)
             db.session.commit()
-            hashed_key = hashlib.sha256('{public_key}{private_key}lol'.format(public_key=request.form['pubkey'], private_key=request.form['privkey'])).hexdigest()
+            hashed_key = hashlib.sha256
+            ('{public_key}{private_key}lol'.format
+                (public_key=request.form['pubkey'],
+                    private_key=request.form['privkey'])).hexdigest()
             return render_template("generated.html", hashed_key=hashed_key)
         else:
             return "Something wrong with data"
     else:
         return "Go to login page again, sorry"
 
+
 @app.route('/display/<special>')
 def display(special):
-    #Query database for keys
+    # Query database for keys
     record = Keys.query.filter(Keys.url == special).first()
-    #Class initialisation for bitmarket API
+    # Class initialisation for bitmarket API
     yolo = Yolo(record.public_key, record.private_key)
     balance = str(yolo.get_balance())
     positions = yolo.swap_list()
-    #Setting cookie
-    #cutoff level
+    # Setting cookie
+    # cutoff level
     cut = yolo.get_cutoff()
-    rsp = make_response(render_template("display.html", balance=balance, interest=interest, positions=positions, cut=cut, special=special))
+    rsp = make_response(render_template("display.html",
+                        balance=balance,
+                        interest=interest,
+                        positions=positions,
+                        cut=cut,
+                        special=special))
     rsp.set_cookie('key', 'value')
     return rsp
+
 
 @app.route('/make_best/<special>')
 def create(special):
 
     record = Keys.query.filter(Keys.url == special).first()
-    #Class initialisation for bitmarket API
+    # Class initialisation for bitmarket API
     yolo = Yolo(record.public_key, record.private_key)
     yolo.cancel_all()
     yolo.make_best()
     return "Made best offer!"
 
+
 @app.route('/set_cookie')
 def cookie_insertion():
     redirect_to_index = redirect('/')
-    response = current_app.make_response(redirect_to_index )
-    response.set_cookie('cookie_name',value='values')
+    response = current_app.make_response(redirect_to_index)
+    response.set_cookie('cookie_name', value='values')
     return response
-
-
-if __name__ == '__main__':
-    if os.path.isfile("test.db"):
-        app.run(host='0.0.0.0', port=5000)
-    else:
-        db.create_all()
-        app.run(host='0.0.0.0', port=5000)
