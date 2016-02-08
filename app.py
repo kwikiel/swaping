ab5c84875e5deed78d76c94481c966a326b2ccbf
Kacper Wikiel <random8902@gmail.com>
2016-02-08 22:55:32 +0100

    TypeError: 'DebugLogger' object is not callable

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-app.logger(logging.StreamHandler(sys.stdout))
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 
