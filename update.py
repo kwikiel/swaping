dcf78f5af2d7600b737cf8dc2c1bea7ff14f04de
Kacper <random8902@gmail.com>
2016-06-16 17:58:06 +0200

    Crontab hack

--- update.py
+++ update.py
@@ -5,13 +5,17 @@
 from app import Keys
 from swaper import Yolo
 
+import time
+
 k = Keys.query.all()
 
-for i in k:
-    try:
-        y = Yolo(key=i.public_key, secret=i.private_key)
-        print y.cancel_all()
-        print y.make_best()
-    except:
-        print "Invalid API KEY"
+for k in range(1,9):
+    for i in k:
+        try:
+            y = Yolo(key=i.public_key, secret=i.private_key)
+            print y.cancel_all()
+            print y.make_best()
+        except:
+            print "Invalid API KEY"
+    time.sleep(59)
 
