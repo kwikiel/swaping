d6dac383088d75f8982a5a044cc490b0bb23997e
Kacper <random8902@gmail.com>
2016-06-18 22:27:54 +0200

    Deleting bad keys

--- update.py
+++ update.py
@@ -3,12 +3,12 @@
 # for each key perform update
 
 from app import Keys
-from swaper import Yolo
+from swaper import Yolo, BitapiException
 
 import time
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 k = Keys.query.all()
 
@@ -17,5 +17,13 @@ for i in k:
         y = Yolo(key=i.public_key, secret=i.private_key)
         print y.cancel_all()
         print y.make_best()
+    except BitapiException as e:
+        print(e.args[0])
+        if e.args[0]['errorMsg'] == 'Invalid API key':
+            print "Key to be removed :_: "
+            kek = Keys.query.filter_by(private_key=i.private_key).delete()
+            db.session.add(kek)
+            db.session.commit()
     except Exception as e:
         logging.exception('wyjebalo sie xD')
+        # print e
